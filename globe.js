71c1d079a611bd4dc5b8bd43359d1b7d35473b2a
Anwaar Ahmed <anwaarahmedjunaid2@gmail.com>
2026-08-18 12:11:46 +0500

    Remove legacy stubs for globe initialization
    Removed legacy stubs for initHomeGlobe and initAppGlobe functions.

--- globe.js
+++ globe.js
@@ -81,7 +81,6 @@ function initSharedGlobe(){
   if(!globeBuilt){ globeBuilt = true; buildGlobe('shared-globe'); }
 }
 
-// Legacy stubs so any remaining calls in app.js don't throw
 function initHomeGlobe(){ initSharedGlobe(); }
 function initAppGlobe(){  initSharedGlobe(); }
 
